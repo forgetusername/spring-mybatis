1a6e8346b434e947a59bec762f373717b3ab4aa3
forgetusername <guowenshi@gmail.com>
2016-10-18 12:05:55 +0800

    Add transaction

---
+++ spring-mybatis-xml/src/main/java/service/UserService.java
@@ -0,0 +1,21 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dao.UserDao;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Transactional
+	public void insert() {
+		userDao.insertUser("test01", "HK");
+		"".substring(11); // will rollback
+	}
+	
+}
---
+++ spring-mybatis-xml/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,21 @@
+package service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("classpath:mybatis.xml")
+public class UserServiceTest {
+
+	@Autowired
+	private UserService userService;
+	
+	@Test
+	public void testInsert() {
+		userService.insert();
+	}
+}
