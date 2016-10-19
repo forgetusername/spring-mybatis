df63bd57bca143ba0090e1b43db908b1b4071790
Kevin <guowenshi@gmail.com>
2016-10-19 11:00:51 +0800

    Update OrderMapper.java

--- spring-mybatis-annotation/src/main/java/dao/mapper/OrderMapper.java
+++ spring-mybatis-annotation/src/main/java/dao/mapper/OrderMapper.java
@@ -2,7 +2,6 @@ package dao.mapper;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Component;
 
 import entity.Order;
 
